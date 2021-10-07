ce2806b9c78bf671ac3bb487b59f0742b3181da8
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-07 10:58:00 +0300

    console.log penkiose eilutėse

--- ciklai.js
+++ ciklai.js
@@ -1,13 +1,79 @@
  // console.log’e, naudojant ciklą atspausdinti penkias eilutes ‘Labas’;
 
+    console.log('Labas');
+    console.log('Labas');
+    console.log('Labas');
+    console.log('Labas');
+    console.log('Labas');
 
- 
+ console.log('-------------');
+
 // console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 1 2 3 4 (vienas skaičius vienoje eilutėje);
+
+ const skaiciai = [0, 1, 2, 3, 4];
+
+    console.log(skaiciai[0]);
+    console.log(skaiciai[1]);
+    console.log(skaiciai[2]);
+    console.log(skaiciai[3]);
+    console.log(skaiciai[4]);
+
+console.log('-------------');
+
+
 // console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje);
+
+const desimtys = [0, 10, 20, 30, 40];
+
+for (let i = 0; i < desimtys.length; i++ ) {
+    console.log(desimtys[i]);
+}
+
+console.log('-------------');
+
+
+
 // console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje);
 // console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 4 3 2 1 0 (vienas skaičius vienoje eilutėje);
 // console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 2 4 6 8 (vienas skaičius vienoje eilutėje, daugybos konsolėje nenaudoti!, if’ų nenaudoti, ciklas for);
 // console.log’e, naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje);
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // console.log’e, naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Paskutinis atspausdintas skaičius turi būti 5;
 // console.log’e, naudojant ciklą atspausdinti atsitiktinius skirtingus skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Ciklas turi pasibaigti tada, kai atsitiktinių skaičių suma viršija 100;
 // console.log’e, naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Paskutinis atspausdintas skaičius turi būti 5 arba 7; Suskaičiuoti kiek ciklų prasisuko;
