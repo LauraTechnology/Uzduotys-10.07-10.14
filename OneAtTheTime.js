64d9630e90760769c7b0b73ff0413ededc5dc56d
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-18 15:08:56 +0300

    Perafrazuoju uždavinio sąlygą

--- OneAtTheTime.js
+++ OneAtTheTime.js
@@ -1,4 +1,8 @@
-// 12. console.log’e, naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje).              Ciklą kartoti kol bus sugeneruoti trys nelyginiai skaičiai;
+// 12. console.log’e, naudojant ciklą atspausdinti 
+// skirtingus atsitiktinius 
+// skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje).              
+// Ciklą kartoti 
+// kol bus sugeneruoti trys nelyginiai skaičiai/ ciklas sukasi, kol rodo 3 nelyginius;
 
 1. - ciklas turi prasisukti 3 kartus
   - mano ciklas prasisuka 1 karta
@@ -11,11 +15,14 @@
            5. - 
 
 
-let kolLoginaTrisNelyginius = -1;
+//let kolLoginaTrisNelyginius = -1;
+//
+//for (let i = 0; i <= 10; i += 2) {
+//  random = Math.floor(Math.random() * 11);
+//  kolLoginaTrisNelyginius += 2;
+//}
+//
+//console.log(kolLoginaTrisNelyginius);
 
-for (let i = 0; i <= 10; i += 2) {
-  random = Math.floor(Math.random() * 11);
-  kolLoginaTrisNelyginius += 2;
-}
 
-console.log(kolLoginaTrisNelyginius);
+
